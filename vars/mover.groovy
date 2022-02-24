064626dbf6123c46525e805a623060f1971ab4ad
Stiven Parada <stiven.parada@hotmail.com>
2022-02-24 11:01:29 -0500

    Modificando groovy

--- vars/mover.groovy
+++ vars/mover.groovy
@@ -1,9 +1,3 @@
-import java.io.IOException
-import java.nio.file.FileSystems
-import java.nio.file.Files
-import java.nio.file.Path
-import java.nio.file.StandardCopyOption
-
-def moverArchivo(){
-    echo 'hola si funciono'
+def moverArchivo{
+    sh "git add ."
 }
