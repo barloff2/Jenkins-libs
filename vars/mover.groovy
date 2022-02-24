41125a9923504c95c31727115b2be7e67158964d
Stiven Parada <stiven.parada@hotmail.com>
2022-02-24 11:38:56 -0500

    Modificando groovy

--- vars/mover.groovy
+++ vars/mover.groovy
@@ -3,16 +3,15 @@ import java.nio.file.FileSystems
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.StandardCopyOption
-package org.foo
 
 def moverArchivo(){
-    shh Path origenPath = FileSystems.default.getPath("/home/stiven/Escritorio/prueba/jenkins/archivo1.txt")
-    shh Path destinoPath = FileSystems.default.getPath("/home/stiven/Escritorio/archivo1.txt")
+    ssh Path origenPath = FileSystems.default.getPath("/home/stiven/Escritorio/prueba/jenkins/archivo1.txt")
+    ssh Path destinoPath = FileSystems.default.getPath("/home/stiven/Escritorio/archivo1.txt")
     
     try {
-       shh Files.move(origenPath, destinoPath, StandardCopyOption.REPLACE_EXISTING)
-       shh echo 'moviendo archivo'
+       ssh Files.move(origenPath, destinoPath, StandardCopyOption.REPLACE_EXISTING)
+       ssh echo 'moviendo archivo'
     }catch (IOException e) {
-        shh echo e.message
+        ssh echo e.message
     }
 }
