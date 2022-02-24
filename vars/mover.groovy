b54a840f9a959098908b19e3d364fdef60b5f54a
Stiven Parada <stiven.parada@hotmail.com>
2022-02-24 17:38:43 -0500

    Prueba numero 2

--- vars/mover.groovy
+++ vars/mover.groovy
@@ -1,11 +1,3 @@
-import java.io.IOException
-import java.nio.file.FileSystems
-import java.nio.file.Files
-import java.nio.file.Path
-import java.nio.file.StandardCopyOption
-import jenkins.model.Jenkins
-import hudson.util.RemotingDiagnostics
-
 def saludar{
     echo "/home/stiven/scripts/script.sh" 
 }
