6d122d1eca901a16b21c70995fa5cb9e6f18985d
Stiven Parada <stiven.parada@hotmail.com>
2022-02-24 10:42:01 -0500

    Modificando groovy

--- vars/mover.groovy
+++ vars/mover.groovy
@@ -5,13 +5,5 @@ import java.nio.file.Path
 import java.nio.file.StandardCopyOption
 
 def moverArchivo(){
-    sh Path origenPath = FileSystems.default.getPath("/home/stiven/Escritorio/archivo1.txt")
-    sh Path destinoPath = FileSystems.default.getPath("/home/stiven/Escritorio/prueba/jenkins/archivo1.txt")
-    
-    try {
-       sh Files.move(origenPath, destinoPath, StandardCopyOption.REPLACE_EXISTING)
-       sh echo 'moviendo archivo'
-    }catch (IOException e) {
-        echo e.message
-    }
+    echo 'hola si funciono'
 }
