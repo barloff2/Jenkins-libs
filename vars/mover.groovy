e9c11b87bad2d899d0f2b621292d3a412a96f89b
Stiven Parada <stiven.parada@hotmail.com>
2022-02-28 10:52:21 -0500

    creando funcion o metodo para crear carpeta

--- vars/mover.groovy
+++ vars/mover.groovy
@@ -1,3 +1,9 @@
+def crearDir(String nombreDir){
+    def ruta = "home/stiven/Escritorio/prueba/${nombreDir}"
+    sh "mkdir ${ruta}"
+}
+
+
 def clonar(){
     def clonar = "https://github.com/barloff2/jenkins.git"
     def directorio = "/home/stiven/Escritorio/prueba/pruebaCop"
@@ -6,7 +12,7 @@ def clonar(){
 
 def mover(){
     def nuevoDir = "mkdir /home/stiven/Documentos/pagina"
-    def copiar = "cp -r home/stiven/Escritorio/prueba/pruebaCop /home/stiven/Documentos/pagina"
+    def copiar = "cp -r home/stiven/Escritorio/prueba/${nombreDir} /home/stiven/Documentos/pagina"
     sh ${nuevoDir}
     sh ${copiar}
 }
