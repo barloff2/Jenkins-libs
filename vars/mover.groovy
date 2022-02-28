600bc5fba3976812ae56e7f810bbe9827494e44d
Stiven Parada <stiven.parada@hotmail.com>
2022-02-28 10:41:36 -0500

    primera prueba

--- vars/mover.groovy
+++ vars/mover.groovy
@@ -1,3 +1,12 @@
-def saludar(){
-    echo 'Hola buenas tardes'
+def clonar(){
+    def clonar = "git branch: 'main', url: 'https://github.com/barloff2/jenkins.git'"
+    def directorio = "/home/stiven/Escritorio/prueba/pruebaCop"
+    sh ${clonar} ${directorio}
+}
+
+def mover(){
+    def nuevoDir = "mkdir /home/stiven/Documentos/pagina"
+    def copiar = "cp -r home/stiven/Escritorio/prueba/pruebaCop /home/stiven/Documentos/pagina"
+    sh ${nuevoDir}
+    sh ${copiar}
 }
