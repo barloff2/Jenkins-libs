7afb0e879397d446a78323ce2163822a37692350
Stiven Parada <barloff2@gmail.com>
2022-03-01 17:21:52 -0500

    Probando de nuevo

--- vars/mover.groovy
+++ vars/mover.groovy
@@ -1,16 +1,3 @@
-String ruta = "C:/Users/Stiven/Documents/repo/desPrueba"
-String destinoDir = "E:/pagina"
-def crearDir(){
-    bat "MD ${ruta} || true"
-}
-
-
-def clonar(){
-    String clonar = "https://github.com/barloff2/jenkins.git"
-    bat "git clone ${clonar} ${ruta}"
-}
-
-def mover(){
-    bat "MD ${destinoDir} || true"
-    bar "mv ${ruta} ${destinoDir}"
+def saludar(){
+    bat echo 'hola buenas tardes'
 }
