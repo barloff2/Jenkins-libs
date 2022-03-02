94768fdde8f20f3bfd882b0bb824c1e58fb8a013
Stiven Parada <barloff2@gmail.com>
2022-03-02 08:38:52 -0500

    probando con powershell

--- vars/mover.groovy
+++ vars/mover.groovy
@@ -1,8 +1,14 @@
 
 def crearDirectorio(){
+    powershell "New-Item C:/destino -itemType Directory"
     powershell "New-Item C:/Users/Stiven/Documents/repo/prueba -itemType Directory"
 }
 
 def clonandoProyecto(){
      powershell "git clone -b test https://github.com/barloff2/jenkins.git C:/Users/Stiven/Documents/repo/prueba"
+}
+
+def moverCarpeta(){
+    powershell "Move-Item -Path C:/Users/Stiven/Documents/repo/prueba -Destination C:/destino
+"
 }
