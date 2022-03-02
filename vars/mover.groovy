6430c6c7aa27e239b8044ad2caee82ecb53b2184
Stiven Parada <barloff2@gmail.com>
2022-03-02 09:08:03 -0500

    probando si funciona con atributos

--- vars/mover.groovy
+++ vars/mover.groovy
@@ -1,20 +1,5 @@
-class Clonacion{
+String ruta = "C:/Users/Stiven/Documents/repo/${BUILD_TAG}"
 
-    def rutaClonacion = "C:/Users/Stiven/Documents/repo/${BUILD_TAG}"
-    def rutaDestino = "C:/destino"
-
-    def crearDirectorio(){
-        powershell "New-Item C:/destino -itemType Directory"
-        powershell "New-Item ${rutaClonacion} -itemType Directory"
-    }
-
-    def clonandoProyecto(){
-        powershell "git clone -b test https://github.com/barloff2/jenkins.git ${rutaClonacion}"
-    }
-
-    def moverCarpeta(){
-        powershell "Move-Item -Path ${rutaClonacion} -Destination ${rutaDestino}"
-    }
-
+def saludar(){
+    echo ruta
 }
-
