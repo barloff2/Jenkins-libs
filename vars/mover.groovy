df9daf540896a17322c78bf33e333f26b6196bb1
Stiven Parada <barloff2@gmail.com>
2022-03-02 08:54:10 -0500

    probando con atributos

--- vars/mover.groovy
+++ vars/mover.groovy
@@ -1,13 +1,14 @@
-
+String rutaClonacion = "C:/Users/Stiven/Documents/repo/${BUILD_TAG}"
+String rutaDestino = "C:/destino"
 def crearDirectorio(){
     powershell "New-Item C:/destino -itemType Directory"
-    powershell "New-Item C:/Users/Stiven/Documents/repo/${BUILD_TAG} -itemType Directory"
+    powershell "New-Item ${rutaClonacion} -itemType Directory"
 }
 
 def clonandoProyecto(){
-     powershell "git clone -b test https://github.com/barloff2/jenkins.git C:/Users/Stiven/Documents/repo/${BUILD_TAG}"
+     powershell "git clone -b test https://github.com/barloff2/jenkins.git ${rutaClonacion}"
 }
 
 def moverCarpeta(){
-    powershell "Move-Item -Path C:/Users/Stiven/Documents/repo/${BUILD_TAG} -Destination C:/destino"
+    powershell "Move-Item -Path ${rutaClonacion} -Destination ${rutaDestino}"
 }
