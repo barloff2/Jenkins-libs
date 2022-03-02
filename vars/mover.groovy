0965164d12e9dd65fdb5b9d4789887ad2552ac4b
Stiven Parada <barloff2@gmail.com>
2022-03-02 14:57:28 -0500

    Insertando variable saludo

--- vars/mover.groovy
+++ vars/mover.groovy
@@ -1,14 +1,18 @@
 def clonandoProyecto(){
     ruta = "${workspace}/${BUILD_TAG}"
-    powershell """
-        echo 'creando carpeta'
+    powershell
+    """
+        echo 'creando carpeta para guardar los archivos...'
         New-Item ${ruta} -itemType Directory
         git clone -b test https://github.com/barloff2/jenkins.git ${ruta}
 
     """
 }
-
 def moverCarpeta(){
 
-    powershell "Move-Item -Path ${ruta} -Destination C:/destino"
+    powershell 
+    """
+        echo 'Moviendo carpeta a destino'
+        Move-Item -Path ${ruta} -Destination C:/destino
+    """
 }
