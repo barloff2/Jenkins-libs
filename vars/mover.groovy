01464cfc08a214dd3699427c97b8f221518c7205
Stiven Parada <barloff2@gmail.com>
2022-03-02 00:05:41 -0500

    probando con clonacion

--- vars/mover.groovy
+++ vars/mover.groovy
@@ -1,5 +1,6 @@
+
 def crearDirectorio(){
-    bat "IF EXIST C:/Users/Stiven/Documents/repo/${JOB_NAME} (echo carpeta existente) ELSE (MD C:/Users/Stiven/Documents/repo/${JOB_NAME})"
+    bat "MD C:/Users/Stiven/Documents/repo/${BUILD_TAG}"
 }
 
 def clonandoProyecto(){
