580190df44a9b1bf49934d8d955091a3acc00ad9
Stiven Parada <barloff2@gmail.com>
2022-03-01 23:08:35 -0500

    Probando con condicional

--- vars/mover.groovy
+++ vars/mover.groovy
@@ -1,5 +1,6 @@
 def crearDirectorio(){
+    def crear = "MD C:/Users/Stiven/Documents/repo/${JOB_NAME}"
     bat """
-        if EXIST C:/Users/Stiven/Documents/repo/${JOB_NAME} (echo La carpeta ya existe) ELSE (MD C:/Users/Stiven/Documents/repo/${JOB_NAME})
+        if EXIST C:/Users/Stiven/Documents/repo/${JOB_NAME} (echo La carpeta ya existe, Continuemos) ELSE (${crear})
     """
 }
