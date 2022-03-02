47a41dd4c3dca6199597fdf503d784393bba4c10
Stiven Parada <barloff2@gmail.com>
2022-03-01 23:04:52 -0500

    Probando con condicional

--- vars/mover.groovy
+++ vars/mover.groovy
@@ -1,5 +1,5 @@
 def crearDirectorio(){
     bat """
-        if C:/Users/Stiven/Documents/repo/${JOB_NAME} (echo exite) ELSE (echo no existe)
+        if EXIST C:/Users/Stiven/Documents/repo/${JOB_NAME} (echo exite) ELSE (echo no existe)
     """
 }
