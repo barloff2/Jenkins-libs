c4c5a57933b0f80bd5cfc82d825d26734e2c707f
Stiven Parada <barloff2@gmail.com>
2022-03-02 14:58:35 -0500

    Creando variable ruta con variables globales

--- vars/mover.groovy
+++ vars/mover.groovy
@@ -1,7 +1,6 @@
 def clonandoProyecto(){
     ruta = "${workspace}/${BUILD_TAG}"
-    powershell
-    """
+    powershell"""
         echo 'creando carpeta para guardar los archivos...'
         New-Item ${ruta} -itemType Directory
         git clone -b test https://github.com/barloff2/jenkins.git ${ruta}
@@ -9,9 +8,7 @@ def clonandoProyecto(){
     """
 }
 def moverCarpeta(){
-
-    powershell 
-    """
+    powershell """
         echo 'Moviendo carpeta a destino'
         Move-Item -Path ${ruta} -Destination C:/destino
     """
