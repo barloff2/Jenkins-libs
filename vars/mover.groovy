f3d6267493dc1670cce51bd55d046217bf443c9f
Stiven Parada <barloff2@gmail.com>
2022-04-25 09:26:28 -0500

    prueba

--- vars/mover.groovy
+++ vars/mover.groovy
@@ -20,7 +20,7 @@ void findFiles(){
         println 'Imprimiendo .path ' + file.path
         println 'Imprimiendo .name ' + file.name
         println 'Imprimiendo file ' + file
-        directory = file.replaceFirst(/${file.name}/, "")
+        directory = file.path.replaceFirst(/${file.name}/, "")
         println 'Imprimiendo Directorio' + directory
         if (!file.path.matches(/.*\.js/)){
             println 'Eliminando '+file.name
