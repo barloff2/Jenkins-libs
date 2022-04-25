53ec35891641bc7ad160a95ef8b27cd7bfc1fad8
Stiven Parada <barloff2@gmail.com>
2022-04-25 09:15:27 -0500

    prueba tostring

--- vars/mover.groovy
+++ vars/mover.groovy
@@ -20,7 +20,7 @@ void findFiles(){
         println 'Imprimiendo .path ' + file.path
         println 'Imprimiendo .name ' + file.name
         println 'Imprimiendo file ' + file
-        directory = file.replace(file.name.toString, "")
+        directory = file.replace(file.name.toString(), "")
         println 'Imprimiendo Directorio' + directory
         if (!file.path.matches(/.*\.js/)){
             println 'Eliminando '+file.name
