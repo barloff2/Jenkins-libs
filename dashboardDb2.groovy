60fac6f532fd71eeaa13ba2564fb803de21af8f8
Stiven Parada <barloff2@gmail.com>
2022-05-02 15:04:12 -0500

    prueba def

--- dashboardDb2.groovy
+++ dashboardDb2.groovy
@@ -1,5 +1,5 @@
 println 'SQL0803N One or more values in the INSERT statement ... not valid because the primary key ... from having duplicate values for the index key. SQLSTATE=23505'
-data = currentBuld.rawBuild.getLog(100).join('\n')
+def data = currentBuld.rawBuild.getLog(100).join('\n')
 searchOnLog(data, 'warning', [
     'SQL0803N one or more values in the INSERT statement'
 ])
