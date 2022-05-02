ba95270c799078acd10b230c951c2a1dd42ed7c6
Stiven Parada <barloff2@gmail.com>
2022-05-02 15:16:28 -0500

    prueba

--- dashboardDb2.groovy
+++ dashboardDb2.groovy
@@ -1,8 +1,6 @@
 def call(){
-    def data = currentBuld.rawBuild.getLog(100).join('\n')
-    searchOnLog(data, 'warning', [
-        'SQL0803N one or more values in the INSERT statement'
-    ])
+    data = currentBuld.rawBuild.getLog(100).join('\n')
+    searchOnLog(data,['SQL0803N one or more values in the INSERT statement'])
 }
 
 void searchOnLog(data, searches){
