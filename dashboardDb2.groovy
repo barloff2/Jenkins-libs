30a5a034730949892b8ed37c903f0df14b6a0e83
Stiven Parada <barloff2@gmail.com>
2022-05-02 15:13:06 -0500

    DEF CALL BIEN HECHO

--- dashboardDb2.groovy
+++ dashboardDb2.groovy
@@ -1,8 +1,9 @@
-def call()
-def data = currentBuld.rawBuild.getLog(100).join('\n')
-searchOnLog(data, 'warning', [
-    'SQL0803N one or more values in the INSERT statement'
-])
+def call(){
+    def data = currentBuld.rawBuild.getLog(100).join('\n')
+    searchOnLog(data, 'warning', [
+        'SQL0803N one or more values in the INSERT statement'
+    ])
+}
 
 void searchOnLog(data, searches){
     searches.each{key, search ->
